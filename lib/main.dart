fa40a4308c491c7f42f6650e4d0b951c8cf37a60
shadowstarr <shadowstar1502@gmail.com>
2021-08-16 14:17:54 +0530

    flutter movie app

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app_flutter/utils/text.dart';
+import 'package:movie_app_flutter/widgets/toprated.dart';
+import 'package:movie_app_flutter/widgets/trending.dart';
+import 'package:movie_app_flutter/widgets/tv.dart';
+import 'package:tmdb_api/tmdb_api.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+      theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.blueAccent),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final String apikey = '4305510d0784e9959a6bab96da50de4a';
+  final String readaccesstoken =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MzA1NTEwZDA3ODRlOTk1OWE2YmFiOTZkYTUwZGU0YSIsInN1YiI6IjYxMTg4NjQ2NGEwYjE5MDA1YzE1Y2Y2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HwogaJHciSA7Ieu-wsckSU-sC7MazrYppt7U5jnXYfo';
+  List trendingmovies = [];
+  List topratedmovies = [];
+  List tv = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadmovies();
+  }
+
+  loadmovies() async {
+    TMDB tmdbWithCustomLogs = TMDB(
+      ApiKeys(apikey, readaccesstoken),
+      logConfig: ConfigLogger(
+        showLogs: true,
+        showErrorLogs: true,
+      ),
+    );
+
+    Map trendingresult = await tmdbWithCustomLogs.v3.trending.getTrending();
+    Map topratedresult = await tmdbWithCustomLogs.v3.movies.getTopRated();
+    Map tvresult = await tmdbWithCustomLogs.v3.tv.getPouplar();
+    print((trendingresult));
+    setState(() {
+      trendingmovies = trendingresult['results'];
+      topratedmovies = topratedresult['results'];
+      tv = tvresult['results'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: modified_text(text: 'Flutter Movie App'),
+          backgroundColor: Colors.transparent,
+        ),
+        body: ListView(
+          children: [
+            TV(tv: tv),
+            TrendingMovies(
+              trending: trendingmovies,
+            ),
+            TopRatedMovies(
+              toprated: topratedmovies,
+            ),
+          ],
+        ));
+  }
+}
